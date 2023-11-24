8a849152a9033bbb76758f81d63c5adfc624ea29
Mustafa <69155967+Hunmbal@users.noreply.github.com>
2023-11-24 22:40:45 +0500

    Add files via upload

--- Wave.m
+++ Wave.m
@@ -1,86 +1,56 @@
-%Author: Mustafa Ali #372704
-%This class will help you create and manipulate periodic waves
+% Author: Mustafa Ali #372704 .... Faizan Hussain #393842
+% Note: our assignment has 2 files "main.m" and "Wave.m"
+% Run "main.m" present in the zip file
+
+
+%% CLASS: WAVE 
+% _______________________________________________________________________
+% the purpose of this class is to reduce redundancy from the code and help
+% manipulate and plot waves
 
 classdef Wave
 
     properties
-        t =0:0.001:18
-        f = []
-        A = []
-        p = []
-        wave = []
-        sample_rate = 4;
-
+        N = 100; % Fundamental period (N) for the discrete signal
+        max_t = 100; %Max value of x-axis (must be >= than N)
+        t 
+        k = [] % Frequency
+        A = [] % Amplitude
+        wave = [] % Final Wave        
     end
 
-    properties (Access = private)
-        r
-        c
-        i
-        lbl
-    end
-    
+ 
     methods
-        function obj = Wave(ff,aa, pp)
-            
-            if (length(ff) == length(aa) && length(ff) == length(pp))
-                obj.f = ff;
+        %% Constructor
+        function obj = Wave(kk,aa)            
+            if (length(kk) == length(aa))
+                obj.k = kk;
                 obj.A = aa;
-                obj.p = pp;           
+                obj.t = 0:1:obj.max_t;
                 obj = obj.update();
             else
                 disp("Wave parameters should be of equal length!");
             end
-
         end
         
-        function res = addwave(obj,ff,aa, pp)
-            res = obj;
-            res.f = [res.f, ff];
-            res.A = [res.A, aa];
-            res.p = [res.p, pp];
-            res = res.update();
-        end
-
+        %% Stem plot
         function [] = plt(obj, r, c, i, lbl)
             subplot(r,c,i);
-            plot(obj.t,obj.wave);
+            stem(obj.t,obj.wave);
             xlabel(lbl);
-            axis([min(obj.t), max(obj.t), utils.getsetMinValue, utils.getsetMaxValue]), grid;
+            axis([0, obj.max_t, min(obj.wave)-3, max(obj.wave)+3]), grid;
         end
 
-        function [] = dplt(obj, r, c, i, lbl)
-            t1 = min(obj.t):1:max(obj.t);
-            y1 = zeros(size(t1));
-
-            for ii = min(t1):max(t1)
-                y1(ii+1) = obj.wave((ii+1)*(1000/obj.sample_rate)-(1000/obj.sample_rate)+1);
-            end
-               
-            subplot(r,c,i);
-            stem(t1,y1);
-            xlabel(lbl);
-            axis([min(obj.t), max(obj.t), utils.getsetMinValue, utils.getsetMaxValue]), grid;
-        end 
-
-
-    end
-
-
-    methods (Access = private)
+        %% Synthesis (Adds all harmonics together and updates the final wave)
         function res = update(obj)
-
             y=zeros(size(obj.t));
-            for row = 1:length(obj.f)
-                y = y + real(obj.A(row) * exp(1i*2*pi*obj.f(row)*obj.t));
+            for row = 1:length(obj.k)
+                y = y + real(obj.A(row) * exp(1j*(2*pi/obj.N)*obj.k(row)*obj.t));
             end
             obj.wave = y;
             res = obj;
-
-
-            utils.getsetMaxValue(max(y));
-            utils.getsetMinValue(min(y));
-        end
+        end    
     end
 end
+
 
--- utils.m
+++ utils.m
@@ -4,69 +4,102 @@
 
 classdef utils
 
+    properties
+        rows = 1;
+        cols = 1;
+    end
 
-    methods (Static)
+    methods
+
+        %init
+        function obj = utils(row,col)
+            obj.rows = row;
+            obj.cols = col;
+        end
     
-        
-        function [] = plt(x, y, r, c, i, lbl)
-            subplot(r,c,i);
+        %% Plot
+        function [] = plot(obj, x, y, i, lbl)
+            subplot(obj.rows,obj.cols,i);
             plot(x,y);
             xlabel(lbl);
-            axis([min(x) max(x) utils.getsetMinValue utils.getsetMaxValue]), grid;
+            axis([min(x) max(x) abs(min(y))*-1.1 max(y)*1.1]), grid;
         end
 
-        function [] = stemplt(x, y, r, c, i, lbl)
-            subplot(r,c,i);
+        % Simple Plot
+        function [] = splot(obj, y,i, lbl)
+            subplot(obj.rows,obj.cols,i);
+            plot(y);
+            xlabel(lbl);
+            %axis([min(x) max(x) abs(min(y))*-1.1 max(y)*1.1]), grid;
+        end
+
+        % Fourier Plot
+        function [] = fplot(obj, x, y, i, lbl)
+            [y_pks,t] = findpeaks(y,x);
+            subplot(obj.rows,obj.cols,i);
             stem(x,y);
             xlabel(lbl);
-            axis([min(x) max(x) utils.getsetMinValue utils.getsetMaxValue]), grid;
+            axis([min(x) t(end)*2 abs(min(y))*-1.1 max(y)*1.2]), grid;
+            %set(gca,'XTick',0:pi/2:2*pi);
+            %set(gca,'XTickLabel',{'0','pi/2','pi','3pi/2','2pi'});
+        end
+
+        %% Stem
+        function [] = stem(obj, x, y, i, lbl)
+            subplot(obj.rows,obj.cols,i);
+            stem(x,y);
+            xlabel(lbl);
+            axis([min(x) max(x) abs(min(y))*-1.1 max(y)*1.1]), grid;
         end 
 
+        %% Find Time Period
+        function res = getPeriod(obj, t,y)
+            [y_pks,x] = findpeaks(y,t);
 
-        function value = getsetMaxValue(x, y)
-            persistent maxValue;
-            
-            if isempty(maxValue)
-                maxValue=5;
-            end
-
-            if nargin > 0
-                if maxValue < x
-                    maxValue = x+3;
+            flag=0;
+            for i = 1:length(x)
+                if flag == 0 && max(y) == y_pks(i)
+                    flag=1;
+                    t1 = i;
+                elseif flag == 1 && max(y) == y_pks(i)
+                    res = x(i)-x(t1);
+                    return;
                 end
             end
-
-            value = maxValue;
-
-            if nargin == 2
-                maxValue = 0;
-            end
+            res = "wave too small";
         end
-        
-        function value = getsetMinValue(x, y)
-            persistent minValue;
-            
-            if isempty(minValue)
-                minValue=5;
-            end
 
-            if nargin > 0
-                if minValue > x
-                    minValue = x-3;
+        %% Get Coefficients
+        function [k,mag,ph] = getCoefficients(~, signal)
+            k = 0:N-1;
+            ak = zeros(1,9);
+            temp=0;
+            for ii=k
+                for n = 1:N*100
+                    temp = temp + signal(n) * exp(-1j*(2*pi/N)*ii*n);
                 end
+                temp = temp/N;
+                ak(ii+1) = temp;
+
+                if ii==0
+                    ak(ii+1) = abs(temp/N);
+                end
+                temp=0;
             end
+            mag = abs(ak);
+            ph = angle(ak);
+        end   
 
-            value = minValue;
+        %% Fourier
+        function [res_y, res_t] = ft(~, t, y)
+            N = length(t)-1;
+            FS = N/max(t);            
+              
+            res_y = round(2*abs(fft(y))/N);
+            res_y = res_y(1:N/2);
 
-            if nargin == 2
-                minValue = 0;
-            end
-        end
-
-        function reset()
-            utils.getsetMinValue(0, 0);
-            utils.getsetMaxValue(0, 0);
-        end
+            res_t = FS*(0:N/2-1)/N;
+        end   
     
     end
 end
