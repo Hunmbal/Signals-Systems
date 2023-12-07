4aa41d06017fc118ccff9716fd70b3efb7fc6397
Mustafa <69155967+Hunmbal@users.noreply.github.com>
2023-12-07 10:17:21 +0500

    Update utils.m

--- utils.m
+++ utils.m
@@ -30,18 +30,28 @@ classdef utils
             subplot(obj.rows,obj.cols,i);
             plot(y);
             xlabel(lbl);
-            %axis([min(x) max(x) abs(min(y))*-1.1 max(y)*1.1]), grid;
+            axis([0 100 min(y) max(y)]), grid;
         end
 
         % Fourier Plot
         function [] = fplot(obj, x, y, i, lbl)
-            [y_pks,t] = findpeaks(y,x);
+            %[y_pks,t] = findpeaks(y,x);
             subplot(obj.rows,obj.cols,i);
             stem(x,y);
             xlabel(lbl);
-            axis([min(x) t(end)*2 abs(min(y))*-1.1 max(y)*1.2]), grid;
+            axis([min(x)*0.8 max(x)*1.2 0 max(y)*1.2]), grid;
             %set(gca,'XTick',0:pi/2:2*pi);
             %set(gca,'XTickLabel',{'0','pi/2','pi','3pi/2','2pi'});
+        end
+
+        % Fourier Plot
+        function [] = piplot(obj, x, y, i, lbl)
+            subplot(obj.rows,obj.cols,i);
+            plot(x,y);
+            xlabel(lbl);
+            axis([min(x) max(x) abs(min(y))*-1.1 max(y)*1.1]), grid;
+            set(gca,'XTick',0:pi/2:2*pi);
+            set(gca,'XTickLabel',{'0','pi/2','pi','3pi/2','2pi'});
         end
 
         %% Stem
@@ -69,37 +79,49 @@ classdef utils
             res = "wave too small";
         end
 
-        %% Get Coefficients
-        function [k,mag,ph] = getCoefficients(~, signal)
+        %% Get Fourier Series Coefficients
+        function [harmonics, coefficients] = getForierSeries(N, signal)
+        
             k = 0:N-1;
-            ak = zeros(1,9);
+            ak = zeros(1,N);
             temp=0;
             for ii=k
-                for n = 1:N*100
-                    temp = temp + signal(n) * exp(-1j*(2*pi/N)*ii*n);
+                for n = 1:N
+                    temp = temp + signal(n) * exp(1j*(2*pi/N)*ii*n);
                 end
                 temp = temp/N;
-                ak(ii+1) = temp;
-
+            
+                ak(ii+1) = abs(temp); 
+            
                 if ii==0
-                    ak(ii+1) = abs(temp/N);
+                    ak(ii+1) = abs(temp); 
                 end
+            
                 temp=0;
             end
-            mag = abs(ak);
-            ph = angle(ak);
-        end   
+            
+            % Extracting non zero values
+            harmonics = k(ak > 0.0001);
+            coefficients = ak(ak > 0.0001);    
+        end  
 
-        %% Fourier
-        function [res_y, res_t] = ft(~, t, y)
-            N = length(t)-1;
+        %% Fourier f domain
+        function [tt, mm, pp] = ft(~, t, y)
+            N = length(t)-1; 
             FS = N/max(t);            
-              
-            res_y = round(2*abs(fft(y))/N);
-            res_y = res_y(1:N/2);
+            
+            res_y = abs(fft(y))/N;
+            res_y = res_y(1:N);
+            res_t = FS*(0:N-1)/N;
+            pp = angle(fft(y));
 
-            res_t = FS*(0:N/2-1)/N;
-        end   
-    
+            index = find(res_y ~= 0);
+            mm = res_y(index);
+            tt = res_t(index);
+            pp = pp(index);
+            
+        end
+
+
     end
 end
